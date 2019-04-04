444e2a9a1d309fb2572be14fc0e46b482da281a3
Pawel Psztyc <jarrodek@gmail.com>
2019-04-03 23:52:28 -0700

    Update: Updating import paths to match web spec

--- uuid-generator.js
+++ uuid-generator.js
@@ -11,7 +11,7 @@ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 License for the specific language governing permissions and limitations under
 the License.
 */
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
+import {PolymerElement} from '../../@polymer/polymer/polymer-element.js';
 
 /**
  * An UUID generator.
