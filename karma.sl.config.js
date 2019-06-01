5ec97642e093a0a967ca8206897a6f600b7e447d
Pawel <jarrodek@gmail.com>
2019-06-01 13:51:49 -0700

    Testing other configuration

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -4,30 +4,30 @@ const slSettings = require('@advanced-rest-client/testing-karma-sl/sl-settings.j
 const createBaseConfig = require('./karma.conf.js');
 
 module.exports = (config) => {
-  const sauceLabs = {
-    testName: 'uuid-generator',
-    // startConnect: true
+  const cnf = {
+    sauceLabs: {
+      testName: 'uuid-generator'
+    }
   };
   if (process.env.TRAVIS) {
     const buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
 
-    config.browserStack.build = buildLabel;
-    // config.browserStack.startTunnel = false;
-    config.browserStack.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+    cnf.browserStack = {};
+    cnf.browserStack.build = buildLabel;
+    // cnf.browserStack.startTunnel = false;
+    cnf.browserStack.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
 
-    config.sauceLabs.build = buildLabel;
-    // config.sauceLabs.startConnect = false;
-    config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
-    config.sauceLabs.recordScreenshots = true;
+    cnf.sauceLabs.build = buildLabel;
+    // cnf.sauceLabs.startConnect = false;
+    cnf.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+    cnf.sauceLabs.recordScreenshots = true;
 
     // Try 'websocket' for a faster transmission first. Fallback to 'polling' if necessary.
-    config.transports = ['websocket', 'polling'];
-    sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+    cnf.transports = ['websocket', 'polling'];
+    cnf.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
   }
   config.set(
-    merge(slSettings(config), createBaseConfig(config), {
-      sauceLabs
-    })
+    merge(slSettings(config), createBaseConfig(config), cnf)
   );
 
   return config;
