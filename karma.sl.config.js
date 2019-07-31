ea3c4f5ebfb05c6ac37867fb6734027a9d97d5ec
Pawel <jarrodek@gmail.com>
2019-07-31 00:15:47 -0700

    fix: fixing npm audit errors by uphgrading to latest ARC standard

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -4,11 +4,16 @@ const slSettings = require('@advanced-rest-client/testing-karma-sl/sl-settings.j
 const createBaseConfig = require('./karma.conf.js');
 
 module.exports = (config) => {
-  const cnf = {
-    sauceLabs: {
-      testName: 'uuid-generator'
-    }
-  };
+  const cnf = slSettings();
+  cnf.sauceLabs.testName = 'uuid-generator';
+  cnf.browsers = [
+    'SL_Chrome',
+    'SL_Chrome-1',
+    'SL_Firefox',
+    'SL_Firefox-1',
+    'SL_Safari',
+    'SL_EDGE'
+  ];
   if (process.env.TRAVIS) {
     const buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
 
@@ -20,9 +25,8 @@ module.exports = (config) => {
     cnf.sauceLabs.build = buildLabel;
     cnf.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
   }
-  config.set(
-    merge(slSettings(config), createBaseConfig(config), cnf)
-  );
+
+  config.set(merge(createBaseConfig(config), cnf));
 
   return config;
 };
